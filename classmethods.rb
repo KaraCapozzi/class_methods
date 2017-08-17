4f034e3ffaf0cec4765aefaf849466bec509ba30
Kara Capozzi <karacapozzi@Karas-MacBook.local>
2017-08-17 16:37:58 -0400

    add bank account class

---
+++ classmethods.rb
@@ -0,0 +1,22 @@
+class BankAccount
+  @@interest_rate
+  @@accounts
+
+  def initialize
+    @@interest_rate = .06
+    @@acccounts = []
+    balance = 0
+  end
+
+  #reader
+  def balance
+
+  end
+
+  #writer
+  def balance
+
+  end
+
+
+end
